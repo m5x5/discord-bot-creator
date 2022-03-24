c16c4bab3bb664bca54dddfa85c7f27d681fb814
m5x5 <michaelpeters5647@gmail.com>
2022-03-24 22:12:54 +0100

    refactor: :art: Remove Bootstrap select from Sidebar
    Form.Select in Sidebar was replaced with custom Select

--- renderer/components/dashboard/DashboardContext.js
+++ renderer/components/dashboard/DashboardContext.js
@@ -79,6 +79,7 @@ export function DashboardProvider({ children }) {
    * @param {number} index
    */
   const updateHandlerIndex = (index) => {
+    console.log({ index });
     let newIndex;
 
     if (handlers[index]) {
--- renderer/components/dashboard/sidebar/index.tsx
+++ renderer/components/dashboard/sidebar/index.tsx
@@ -1,6 +1,6 @@
-import { Card, Form, Nav } from 'react-bootstrap';
 import { styled } from '../../../stitches.config';
 import Button from '../../core/Button';
+import Select from '../../core/Select';
 import { useDashboardContext } from '../DashboardContext';
 import SidebarBotControls from './Controls';
 import ControlsContextProvider from './Controls/Context';
@@ -11,17 +11,23 @@ const Items = styled('div', {
   maxWidth: '100vw',
   height: '100vh',
 
-  '>.card-body': {
-    backgroundColor: '$gray800',
-  },
-
   '@bp1': {
     height: '100vh',
     display: 'flex',
     flexFlow: 'column',
+  },
+});
 
-    '>.card-body': {
-      flexGrow: '1',
+const Body = styled('div', {
+  display: 'flex',
+  padding: '$2',
+
+  '@bp1': {
+    flexGrow: '1',
+    backgroundColor: '$gray800',
+
+    '> .select': {
+      display: 'none',
     },
   },
 });
@@ -32,7 +38,15 @@ const Footer = styled('div', {
   backgroundColor: '$gray900',
 });
 
-export default function Sidebar({ selected }) {
+const Nav = styled('div', {
+  display: 'none',
+  '@bp1': {
+    display: 'flex',
+    flexFlow: 'column',
+  },
+});
+
+export default function Sidebar() {
   const {
     mode,
     handlers,
@@ -43,40 +57,36 @@ export default function Sidebar({ selected }) {
     addHandler,
   } = useDashboardContext();
 
+  const list = commands
+    .concat(events)
+    .map((c, i) => ({ value: +i, label: c?.name }));
+  const selected = list.find((c) => c.value === handlerIndex) || list[0];
+  console.log({ selected });
+
   return (
     <>
       <SidebarItems />
       {['command', 'event'].includes(mode) && (
         <Items>
-          <Card.Body className="px-1 py-1 overflow-auto">
-            <Nav variant="pills" className="flex-column d-md-block d-none">
+          <Body>
+            <Nav>
               {handlers.map((d, i) => (
                 <SidebarListItem
                   color={handlerIndex === i ? 'active' : 'initial'}
                   onClick={() => updateHandlerIndex(i)}
                   i={i}
                   d={d}
-                ></SidebarListItem>
+                />
               ))}
             </Nav>
-            <Form.Group className="d-md-none">
-              <Form.Select
-                id="command"
-                value={selected}
-                onChange={(e) => updateHandlerIndex(e.target.value)}
-              >
-                {commands?.concat(events || []).map((c, i) => (
-                  <option
-                    key={'select-' + c?.name + '-' + i}
-                    onClick={() => updateHandlerIndex(i)}
-                    value={i}
-                  >
-                    {c?.name}
-                  </option>
-                ))}
-              </Form.Select>
-            </Form.Group>
-          </Card.Body>
+            <Select
+              id="command"
+              value={selected}
+              onChange={updateHandlerIndex}
+              options={list}
+              className="select"
+            />
+          </Body>
           <Footer>
             <ControlsContextProvider>
               <SidebarBotControls />
