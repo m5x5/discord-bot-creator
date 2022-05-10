fb2561ea0a6d70deb7237ed6cb2b6b714f16deb4
m5x5 <michaelpeters5647@gmail.com>
2022-05-10 11:08:00 +0200

    feat(Actions): :sparkles: Convert "Add Image Attachment to Embed" to "Create Image Attachment"

---
+++ resources/actions/create_image_attachment.js
@@ -0,0 +1,34 @@
+module.exports = {
+  name: 'Create Image Attachment',
+  section: 'Embed Message',
+
+  fields: [],
+  form: {
+    imageVarName: {
+      type: 'text',
+      title: 'Image URL',
+    },
+    filename: {
+      type: 'text',
+      title: 'Image Name',
+      placeholder: 'image.png',
+      description: 'The name with which the image will be saved in the server.',
+    },
+    storage: {
+      type: 'storage',
+      variant: 1,
+      title: 'Storage',
+      inline: true,
+    },
+    varName: {
+      type: 'text',
+      title: 'Variable Name',
+      placeholder: 'Embed Variable Name',
+      inline: true,
+      if: {
+        field: 'embedstorage',
+        greaterThan: 0,
+      },
+    },
+  },
+};
